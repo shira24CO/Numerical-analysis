88eeac79b482d10eaf6dbdb23d1ffcb62d0de88b
Ilanshk <93129754+Ilanshk@users.noreply.github.com>
2022-03-10 18:34:47 +0200

    changeIlan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
 exp = abs(3.0*(4.0/3.0-1)-1)
 def machine_epsilon():
-    x = 1
-    while (1 + x) != 1:
+    epsilon_machine = 1
+    while (1 + epsilon_machine) != 1:
+         epsilon =epsilon_machine
+         epsilon_machine = epsilon_machine/2
+
