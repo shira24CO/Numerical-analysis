06ac0f387836ad5a392d0695c3139661de2ceedc
eli ben aharon <eli14741@gmail.com>
2022-03-10 19:25:47 +0200

    changeeli

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ def machine_epsilon():
     while (1 + epsilon_machine) != 1:
          epsilon =epsilon_machine
          epsilon_machine = epsilon_machine/2
+    return epsilon
+
+print(machine_epsilon())
 
