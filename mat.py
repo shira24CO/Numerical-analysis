a58df2282801ae2df9c8e5bca7996271c8e3c609
eli ben aharon <eli14741@gmail.com>
2022-03-22 18:58:58 +0200

    eli2

--- mat.py
+++ mat.py
@@ -1,5 +1,6 @@
 import math
 
+
 def printmat(matrix):
     for i in matrix:
         for j in i:
@@ -22,69 +23,70 @@ def multiplyMatrix(mat1,mat2):
 def solvLU(A):
     size = len(A)  # Give us total of lines
     # (1) Extract the b vector
-    bv = [0 for i in range(size)]
+    b = [0 for i in range(size)]
     for i in range(size):
-        bv[i] = A[i][size]
-    print("b= ",bv)
+        b[i] = A[i][size]
+    print("b= ",b)
 
-    # (2) Fill L Umat and its diagonal with 1
+    # (2) Fill L matrix and its diagonal with 1
     Lmat = [[0 for i in range(size)] for i in range(size)]
     for i in range(size):
         Lmat[i][i] = 1
 
-    # (3) Fill U Umat
+    # (3) Fill U matrix
     Umat = [[0 for i in range(size)] for i in range(size)]
     for i in range(size):
         for j in range(0, size):
             Umat[i][j] = A[i][j]
-#Filling of an elementary matrix
-    elem =[[0 for i in range(size)] for i in range(size)]
-    for i in range(size):
-        for j in range(0, size):
-            elem[i][j] = Lmat[i][j]
-
+#
     size = len(Umat)
+
+#----------------------------------------------------
     # (4) Find both U and L matrices
     for i in range(0, size):  # for i in [0,1,2,..,n]
         # (4.1) Find the maximun value in a column in order to change lines
+
         maxElem = abs(Umat[i][i])
         maxRow = i
+
         for k in range(i + 1, size):  # Interacting over the next line
             if (abs(Umat[k][i]) > maxElem):
                 maxElem = abs(Umat[k][i])  # Next line on the diagonal
                 maxRow = k
+        elem = [[0 for i in range(size)] for i in range(size)]
+        for i in range(size):
+            elem[i][i] = 1
         # (4.2) Swap the rows pivoting the maxRow, i is the current row
-        #s
         for k in range(i, size):
-            for k in range(i, size):
-                # Interacting column by column
-                tmp = elem[maxRow][k]
-                elem[maxRow][k] = elem[i][k]
-                elem[i][k] = tmp
+            # Interacting column by column
+            tmp = elem[maxRow][k]
+            elem[maxRow][k] =elem[i][k]
+            elem[i][k] = tmp
 
-            print("ELEM")
-            printmat(elem)
-            print("Umatrix")
-            printmat(Umat)
+        print("ELEM")
+        printmat(elem)
+        print("matrix")
+        printmat(Umat)
+        Umat = multiplyMatrix(elem, Umat)
+        print("resalt")
+        printmat(Umat)
+        elem = Lmat
+        print("ID")
+        printmat(elem)
 
-          # (4.3) Subtract lines
-            for k in range(i + 1, size):
-                piv = -Umat[k][i] / float(Umat[i][i])
-                Lmat[k][i] = piv
-                Umat = multiplyMatrix(elem, Umat)
-                print("resalt")
-                printmat(Umat)
-                print("pivot",piv)
-#-------------------------------------------------------------------------    עובד טוב צריך לבדוק למה piv שזה pivot משתנה
+'''        # (4.3) Subtract lines
+        for k in range(i + 1, size):
+            c = -matrix[k][i] / float(matrix[i][i])
+            ID[k][i] = c  # (4.4) Store the multiplier
+            for j in range(i, size):
+                matrix[k][j] += c * matrix[i][j]  # Multiply with the pivot line and subtract
 
-        # (4.4) Store the multiplier
-        for j in range(i, size):
-            Umat[k][j] += piv * Umat[i][j]  # Multiply with the pivot line and subtract       # (4.5) Make the rows bellow this one zero in the current column
+        # (4.5) Make the rows bellow this one zero in the current column
         for k in range(i + 1, size):
             matrix[k][i] = 0
 
     size = len(ID)
-'''
+
     # (5) Perform substitutioan Ly=b
     y = [0 for i in range(size)]
     for i in range(0, size, 1):
@@ -101,6 +103,6 @@ def solvLU(A):
         for k in range(i - 1, -1, -1):
             matrix[i] -= x[i] * matrix[i][k]'''
 
-#    r'''eturn x'''
+#    return x
 A = [[1,2,3,0],[4,5,6,0],[7,8,8,0]]
 solvLU(A)
